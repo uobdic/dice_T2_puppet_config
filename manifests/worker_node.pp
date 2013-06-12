8a130b753547657304959f9cd81e16439e8fef12
kreczko <lkreczko@gmail.com>
2013-06-12 11:11:08 +0100

    adding parameters to the worker node

--- manifests/worker_node.pp
+++ manifests/worker_node.pp
@@ -1,14 +1,20 @@
-class site::worker_node inherits site::params {
+class site::worker_node (
+  $cvmfs_version = $site::params::cvmfs_version,
+  $cvmfs_quota_limit = $site::params::cvmfs_quota_limit,
+  $cvmfs_http_proxy = $site::params::cvmfs_http_proxy,
+  $cvmfs_server_url = $site::params::cvmfs_server_url,
+  $cvmfs_cache_base = $site::params::cvmfs_cache_base,
+) inherits site::params {
 
   class{'cvmfs::install':
-    cvmfs_version => $site::params::cvmfs_version,
+    cvmfs_version => $cvmfs_version,
   }
   
   class{'cvmfs::config':
-    cvmfs_quota_limit => $site::params::cvmfs_quota_limit,
-    cvmfs_http_proxy => $site::params::cvmfs_http_proxy,
-    cvmfs_server_url => $site::params::cvmfs_server_url,
-    cvmfs_cache_base => $site::params::cvmfs_cache_base,
+    cvmfs_quota_limit => $cvmfs_quota_limit,
+    cvmfs_http_proxy => $cvmfs_http_proxy,
+    cvmfs_server_url => $cvmfs_server_url,
+    cvmfs_cache_base => $cvmfs_cache_base,
   }
    class{'cvmfs::service':}
   #  class {'cvmfs:'}
