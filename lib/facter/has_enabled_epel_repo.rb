c2542ce131807acc59e60a76d4096f1574798fa0
kreczko <lkreczko@gmail.com>
2013-06-25 13:00:29 +0100

    fixing EPEL fact definition

--- lib/facter/has_enabled_epel_repo.rb
+++ lib/facter/has_enabled_epel_repo.rb
@@ -5,13 +5,7 @@
 Facter.add(:has_enabled_epel_repo) do
   setcode do
     begin
-      any_enabled = Facter::Util::Resolution.exec("less /etc/yum.repos.d/epel*.repo | grep enabled=1 | wc -l")
-      case any_enabled
-      when 0
-        "no"
-      else
-        "yes"
-      end
+      Facter::Util::Resolution.exec("less /etc/yum.repos.d/epel*.repo | grep enabled=1 | wc -l")
     rescue Exception
       Facter.debug('epel repository not found')
     end
