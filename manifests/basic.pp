623cbec06158a0233961afef9401e51d4da22d30
kreczko <lkreczko@gmail.com>
2013-08-09 17:22:58 +0100

    making basic.pp pass puppet-lint

--- manifests/basic.pp
+++ manifests/basic.pp
@@ -1,31 +1,31 @@
 class site::basic (
-  $cluster = $site::params::cluster, 
+  $cluster = $site::params::cluster,
   $yum_repositories = [],
-  $nameserver = [], 
+  $nameserver = [],
   $search = [],
   $packages = {
-    "nano" => {},
-    "yum" => {},
-    "git" => {},
-    "wget" => {},
-    "mlocate" => {},
+    'nano' => {},
+    'yum' => {},
+    'git' => {},
+    'wget' => {},
+    'mlocate' => {},
   },
   $firewall_rules = {},
   $use_firewall = false,
 ) inherits site::params {
-  
+
   File {#defaults for files
     mode   => 644,
-    owner  => "root",
-    group  => "root",
-    ensure => "present",
-  }  
-    
+    owner  => 'root',
+    group  => 'root',
+    ensure => 'present',
+  }
+
   #############################
   # yum repositories
   #############################
   class { 'site::yum_repositories':
-    repositories => $yum_repositories,
+    repositories => $::yum_repositories,
   }
 
   #############################
@@ -34,44 +34,44 @@ class site::basic (
   package { 'puppet': ensure => latest, }
 
   service { 'puppet':
-    ensure => "running",
+    ensure => 'running',
     enable => true,
   }
 
   file { '/etc/puppet/puppet.conf':
-    notify  => Service["puppet"],
-    content => template("site/puppet.conf.erb"),
-    require => Package["puppet"],
+    notify  => Service['puppet'],
+    content => template('site/puppet.conf.erb'),
+    require => Package['puppet'],
   }
 
   file { '/etc/puppet/auth.conf':
-    notify  => Service["puppet"],
-    source  => "puppet:///modules/site/auth.conf",
-    require => Package["puppet"],
+    notify  => Service['puppet'],
+    source  => 'puppet:///modules/site/auth.conf',
+    require => Package['puppet'],
   }
-  
+
   #############################
   # basic packages
   #############################
   motd::file { 'mine': template => "site/motd_${cluster}.erb" }
-  
+
   $package_defaults = {
     'ensure' => installed,
   }
-  create_resources('package', $packages, $package_defaults)
+  create_resources('package', $::packages, $package_defaults)
 
   file { '/root/.bash_profile':
-    source  => "puppet:///modules/site/bash_profile",
-    require => Package["puppet"],
+    source  => 'puppet:///modules/site/bash_profile',
+    require => Package['puppet'],
   }
 
   class { 'site::resolvconf':
-    nameserver => $nameserver,
-    search     => $search,
+    nameserver => $::nameserver,
+    search     => $::search,
   }
-  
 
-  if $use_firewall == true {
-    class { 'site::firewall': rules => $firewall_rules, }    
+
+  if $::use_firewall == true {
+    class { 'site::firewall': rules => $::firewall_rules, }
   }
 }
