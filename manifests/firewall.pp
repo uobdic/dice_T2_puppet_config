6588ee17b35fac77de05a271c5ed2d3968082903
kreczko <lkreczko@gmail.com>
2013-08-09 17:34:18 +0100

    making firewall.pp compliant with puppet-lint

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -2,19 +2,28 @@ class site::firewall (
   $rules = {
   }
 ) {
-  $ipv4_file = $operatingsystem ? {
-    "debian"          => '/etc/iptables/rules.v4',
+  $ipv4_file      = $operatingsystem ? {
+    'debian'          => '/etc/iptables/rules.v4',
     /(RedHat|CentOS)/ => '/etc/sysconfig/iptables',
     default           => '/etc/sysconfig/iptables',
   }
 
-  exec { "purge default firewall":
-    command => "/sbin/iptables -F && /sbin/iptables-save > $ipv4_file && /sbin/service iptables restart",
-    onlyif  => "/usr/bin/test `/bin/grep \"Firewall configuration written by\" $ipv4_file | /usr/bin/wc -l` -gt 0",
+  # for readability and to keep lines below 80 characters (puppet-lint)
+  $flush_tables   = 'iptables -F'
+  $save_tables    = "iptables-save > $ipv4_file"
+  $restart_tables = 'service iptables restart'
+  $search_for     = 'Firewall configuration written by'
+
+  exec { 'purge default firewall':
+    command => "$flush_tables && $save_tables && $restart_tables",
+    onlyif  => "test `grep \"$search_for\" $ipv4_file | wc -l` -gt 0",
+    path    => [
+      '/sbin',
+      '/usr/bin'],
     user    => 'root',
   }
 
-  resources { "firewall": purge => true }
+  resources { 'firewall': purge => true }
 
   Firewall {
     before  => Class['site::firewall::post'],
