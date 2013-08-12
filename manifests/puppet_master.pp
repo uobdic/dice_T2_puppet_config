26d7c4bacb567f7d753e8230aacaedea4502a2aa
kreczko <lkreczko@gmail.com>
2013-08-12 11:23:06 +0100

    adding date to backup, adding cron job for cleaning old files

--- manifests/puppet_master.pp
+++ manifests/puppet_master.pp
@@ -12,11 +12,18 @@ class site::puppet_master (
     "${backup_folder}/puppet/",
     "${backup_folder}/root/"]
 
-  file { $backup_directories: ensure => "directory", }
+  file { $backup_directories: ensure => 'directory', }
 
   # cron job for dumping the PostgreSQL database
   cron { postgresql_backup:
-    command => "/usr/bin/sudo -u foreman /usr/bin/pg_dump &> ${backup_folder}/foreman/pg_dump.txt",
+    command => "/usr/bin/sudo -u foreman /usr/bin/pg_dump &> ${backup_folder}/foreman/pg_dump_`date +%d_%m_%Y_%H.%M`.txt",
+    user    => root,
+    hour    => '*/8',
+  }
+  
+  $keep_for_n_days = 3
+  cron { postgresql_backup_cleanup:
+    command => "find ${backup_folder}/foreman/pg_dump* -mtime +3 -exec rm {} \\;",
     user    => root,
     hour    => '*/8',
   }
