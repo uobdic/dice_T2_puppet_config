8cbb199ecc3c2a1e142d6438fa2d8014050bedc8
Luke Kreczko <lkreczko@gmail.com>
2013-08-14 16:13:54 +0100

    adding python helper script for iptables->yaml (for DROPs)

---
+++ files/iptables2yaml.py
@@ -0,0 +1,12 @@
+rules = open('rules.txt')
+i = 200
+for rule in rules.readlines():
+    if rule.startswith('#'):
+        continue
+    else:
+        parts = rule.split(' ')
+        range = parts[3]
+        print '  "%d Drop known troublesome range (%s)":' % (i, range)
+        print '    source:', '"%s"' % range
+        print '    action: drop'
+        i += 1
