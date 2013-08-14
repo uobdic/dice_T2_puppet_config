25c5edaee2a45cbaf554c117c481d58c997bb51f
Luke Kreczko <lkreczko@gmail.com>
2013-08-14 15:36:19 +0100

    simplifying firewall rules

--- manifests/firewall/dice.pp
+++ manifests/firewall/dice.pp
@@ -1,41 +1,34 @@
 class site::firewall::dice {
-  firewall { '100 SSH from UOB':
-    proto  => 'tcp',
-    action => 'accept',
-    dport  => 22,
-    source => '137.222.0.0/16',
-  }
-
-  firewall { '101 SSH from CSE':
+  firewall { '100 SSH from CSE':
     proto  => 'tcp',
     action => 'accept',
     dport  => 22,
     source => '92.234.12.153/16',
   }
 
-  firewall { '102 SSH from VPN':
+  firewall { '101 SSH from VPN':
     proto  => 'tcp',
     action => 'accept',
     dport  => 22,
     source => '172.21.0.0/16',
   }
 
-  firewall { '103 For UB IS backups':
+  firewall { '102 Trust UoB network':
     proto  => 'all',
     action => 'accept',
-    source => '137.222.10.17',
+    source => '137.222.0.0/16',
   }
 
-  firewall { '104 Trust internal network':
+  firewall { '103 Trust internal network':
     proto  => 'all',
     action => 'accept',
     source => '10.132.0.0/16',
   }
 
-  firewall { '105 drop all-systems.mcast.net':
+  firewall { '104 drop all-systems.mcast.net':
     proto       => 'igmp',
     action      => 'drop',
     destination => '224.0.0.1',
   }
-  
+
 }
