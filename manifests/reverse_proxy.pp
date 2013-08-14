8ae66c65042c8bd338c044a655e2c8d8bb7c4c8f
Luke Kreczko <lkreczko@gmail.com>
2013-08-14 11:43:18 +0100

    adding rules for reverse proxy

--- manifests/reverse_proxy.pp
+++ manifests/reverse_proxy.pp
@@ -1,5 +1,45 @@
-class site::reverse_proxy {
-  firewall { '101 forward port 80 to 8000':
+class site::reverse_proxy (
+  $service_ports = {
+  }
+) {
+  firewall { '100 snat for network DICE':
+    chain    => 'POSTROUTING',
+    jump     => 'MASQUERADE',
+    proto    => 'all',
+    outiface => "em2",
+    source   => '10.132.0.0/16',
+    table    => 'nat',
+  }
+
+  firewall { '101 snat for network DICE':
+    chain    => 'POSTROUTING',
+    jump     => 'SNAT',
+    proto    => 'all',
+    outiface => "em2",
+    tosource => $::ipaddress_em2,
+    table    => 'nat',
+  }
+
+  firewall { '102 forwarding from internal network':
+    chain    => 'FORWARD',
+    action   => 'accept',
+    proto    => 'all',
+    outiface => "em2",
+    source   => '10.132.0.0/16',
+  }
+
+  firewall { '103 forwarding to internal network':
+    chain       => 'FORWARD',
+    action      => 'accept',
+    proto       => 'all',
+    iniface     => "em2",
+    state       => [
+      'ESTABLISHED',
+      'RELATED'],
+    destination => '10.132.0.0/16',
+  }
+
+  firewall { '150 forward port 80 to 8000':
     table   => 'nat',
     chain   => 'PREROUTING',
     jump    => 'REDIRECT',
@@ -8,21 +48,12 @@ class site::reverse_proxy {
     toports => 443
   }
 
-  firewall { '100 snat for network DICe':
-    chain    => 'POSTROUTING',
-    jump     => 'MASQUERADE',
-    proto    => 'all',
-    outiface => "em2",
-    source   => '10.132.0.0/16',
-    table    => 'nat',
+  $defaults = {
+    'action' => 'accept',
+    proto    => 'tcp',
+    state    => 'NEW',
   }
-  
-  firewall { '101 snat for network DICe':
-    chain    => 'POSTROUTING',
-    jump     => 'SNAT',
-    proto    => 'all',
-    outiface => "em2",
-    tosource => '10.132.0.0/16',
-    table    => 'nat',
-  }
+
+  # ports for services
+  create_resources('firewall', $service_ports, $defaults)
 }
