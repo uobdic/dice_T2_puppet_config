a00a5d47f9bbb79aa0a4e02c1860e79b3927976b
Luke Kreczko <lkreczko@gmail.com>
2013-08-20 11:01:25 +0100

    adding default routes for HEP and T2

--- manifests/firewall/tiertwo.pp
+++ manifests/firewall/tiertwo.pp
@@ -1,3 +1,13 @@
 class site::firewall::tiertwo {
+  firewall { '100 Trust UoB network':
+    proto  => 'all',
+    action => 'accept',
+    source => '137.222.0.0/16',
+  }
 
+  firewall { '101 drop all-systems.mcast.net':
+    proto       => 'igmp',
+    action      => 'drop',
+    destination => '224.0.0.1',
+  }
 }
