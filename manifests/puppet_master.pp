5cac7526630a192e993ba6c9614d2f5e63051003
Luke Kreczko <lkreczko@gmail.com>
2013-08-22 17:54:29 +0100

    just formatting change after real change (213db5c). Keeping separate for easier understanding.

--- manifests/puppet_master.pp
+++ manifests/puppet_master.pp
@@ -22,34 +22,36 @@ class site::puppet_master (
 
   # cron job for dumping the PostgreSQL database
   cron { postgresql_backup:
-    command => "pg_dump | gzip &> ${backup_folder}/foreman/pg_dump_`date +\\%d_\\%m_\\%Y_\\%H.\\%M`.gz",
-    user    => foreman,
-    hour    => '*/8',
-    minute  => 0,
-    environment => ['/usr/bin', '/bin'],
+    command     => "pg_dump | gzip &> ${backup_folder}/foreman/pg_dump_`date +\\%d_\\%m_\\%Y_\\%H.\\%M`.gz",
+    user        => foreman,
+    hour        => '*/8',
+    minute      => 0,
+    environment => [
+      '/usr/bin',
+      '/bin'],
   }
 
   cron { postgresql_backup_cleanup:
-    command => "find ${backup_folder}/foreman/pg_dump* -mtime +3 -exec rm {} \\;",
-    user    => root,
-    hour    => '*/8',
-    minute  => 0,
+    command     => "find ${backup_folder}/foreman/pg_dump* -mtime +3 -exec rm {} \\;",
+    user        => root,
+    hour        => '*/8',
+    minute      => 0,
     environment => ['/bin'],
   }
 
   cron { puppet_backup:
-    command => "tar czf ${backup_folder}/puppet/puppet_`date +\\%d_\\%m_\\%Y_\\%H.\\%M`.tar.gz /etc/puppet ",
-    user    => root,
-    hour    => '*/8',
-    minute  => 0,
+    command     => "tar czf ${backup_folder}/puppet/puppet_`date +\\%d_\\%m_\\%Y_\\%H.\\%M`.tar.gz /etc/puppet ",
+    user        => root,
+    hour        => '*/8',
+    minute      => 0,
     environment => ['/bin'],
   }
 
   cron { puppet_backup_cleanup:
-    command => "find ${backup_folder}/puppet/puppet_* -mtime +3 -exec rm {} \\;",
-    user    => root,
-    hour    => '*/8',
-    minute  => 0,
+    command     => "find ${backup_folder}/puppet/puppet_* -mtime +3 -exec rm {} \\;",
+    user        => root,
+    hour        => '*/8',
+    minute      => 0,
     environment => ['/bin'],
   }
 }
