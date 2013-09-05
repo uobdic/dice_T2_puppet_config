c7ccb1cd787a8e2c0ce494a9a8b4f99aba63b327
Luke Kreczko <lkreczko@gmail.com>
2013-09-05 09:55:41 +0100

    adding 2nd backup server to firewall rules

--- manifests/backup/firewall.pp
+++ manifests/backup/firewall.pp
@@ -1,8 +1,18 @@
 class site::backup::firewall {
-  firewall { '200 For UB IS backups':
+  firewall { '200 For UoB IS backups':
     action => 'accept',
     proto  => 'all',
     source => '137.222.10.17',
+    state  => 'NEW',
+    dport  => '7937-9936',
+    notify => Service['networker'], # restart networker after applying this
+                                    # firewall rule
+  }
+
+  firewall { '201 For UoB IS backups':
+    action => 'accept',
+    proto  => 'all',
+    source => '137.222.9.60',
     state  => 'NEW',
     dport  => '7937-9936',
     notify => Service['networker'], # restart networker after applying this
