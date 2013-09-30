0aa9c7d04e9691be9e941821c892159541ec573d
Luke Kreczko <lkreczko@gmail.com>
2013-09-30 11:24:02 +0100

    adding kerberos configuration for user set-up

--- manifests/basic_user.pp
+++ manifests/basic_user.pp
@@ -1,15 +1,21 @@
 class site::basic_user (
   $user_accounts = $site::params::user_accounts,
-  $user_groups = $site::params::user_groups,
-) inherits site::params {
-  
-  class{'site::create_groups':
+  $user_groups   = $site::params::user_groups,) inherits site::params {
+  package { 'pam_krb5': ensure => present, }
+
+  file { '/etc/krb5.conf':
+    ensure => present,
+    source => 'puppet:///modules/site/krb5.conf',
+  }
+
+  class { 'site::create_groups':
     groups => $user_groups,
   }
-  
-  class{'site::create_accounts':
+
+  class { 'site::create_accounts':
     accounts => $user_accounts,
   }
-  
-  Class['site::create_groups'] -> Class['site::create_accounts'] 
+
+  Package['pam_krb5'] -> File['/etc/krb5.conf'] -> Class['site::create_groups'] 
+  -> Class['site::create_accounts']
 }
