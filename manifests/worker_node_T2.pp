a50bd1c15675a7970ecb28bc95216f638c18dc2b
Luke Kreczko <lkreczko@gmail.com>
2013-10-18 12:25:27 +0100

    adding new site-config for T2 WNs

---
+++ manifests/worker_node_T2.pp
@@ -0,0 +1,16 @@
+class site::worker_node_T2 {
+  file { [
+    '/opt/cvmfs',
+    '/opt/cvmfs/cms.cern.ch',
+    '/opt/cvmfs/cms.cern.ch/SITECONF',
+    '/opt/cvmfs/cms.cern.ch/SITECONF/local',
+    '/opt/cvmfs/cms.cern.ch/SITECONF/local/JobConfig',
+    '/opt/cvmfs/cms.cern.ch/SITECONF/local/PhEDEx',
+    ]:
+    ensure => directory,
+  } ->
+  file { '/opt/cvmfs/cms.cern.ch/SITECONF/local/JobConfig/site-local-config.xml':
+    ensure => present,
+    source => 'puppet:///modules/site/site-local-config.T2.xml',
+  }
+}
