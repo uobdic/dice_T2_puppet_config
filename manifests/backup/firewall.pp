0bacdc95a9a4cb4074af49118618f6ab5f6b415c
Luke Kreczko <lkreczko@gmail.com>
2013-11-22 16:34:53 +0000

    fix for incorrect protocol for firewall rule

--- manifests/backup/firewall.pp
+++ manifests/backup/firewall.pp
@@ -1,7 +1,7 @@
 class site::backup::firewall {
   firewall { '200 For UoB IS backups':
     action => 'accept',
-    proto  => 'all',
+    proto  => ['tcp', 'udp'],
     source => '137.222.10.17',
     state  => 'NEW',
     dport  => '7937-9936',
@@ -11,7 +11,7 @@ class site::backup::firewall {
 
   firewall { '201 For UoB IS backups':
     action => 'accept',
-    proto  => 'all',
+    proto  => ['tcp', 'udp'],
     source => '137.222.9.60',
     state  => 'NEW',
     dport  => '7937-9936',
