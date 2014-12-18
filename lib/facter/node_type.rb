67657cd09f2b1b4904c2e71d94ea62a462749daf
kreczko <lkreczko@googlemail.com>
2014-12-18 10:49:13 +0000

    added CouchDB node type

--- lib/facter/node_type.rb
+++ lib/facter/node_type.rb
@@ -12,6 +12,8 @@ Facter.add(:node_type) do
       type = "vm-guest"
     elsif hostname.start_with? "dice-io-"
       type = "io-node"
+    elsif hostname.start_with? "bc-"
+      type = "couchdb-node"
     elsif hostname.start_with? "nn-" or hostname.start_with? "jt-"
       type = "headnode"
     elsif hostname.start_with? "lcg"
