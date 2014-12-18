bd8bebbf22072de01784652f6d164ed874625d83
kreczko <lkreczko@googlemail.com>
2014-12-18 12:41:35 +0000

    bugfix for setting defaults

--- manifests/users.pp
+++ manifests/users.pp
@@ -21,16 +21,21 @@ class site::users (
   }
   create_resources(group, $groups, $group_defaults)
 
-  $defaults = {
-    'ensure'       => present,
-    'shell'        => '/bin/bash',
-    'password'     => '!!',
-    'create_group' => false,
-  }
-
-  if ($::node_type != 'vm-guest' and $::node_type != 'worker') {
+  if ($::node_type == 'vm-guest' or $::node_type == 'worker') {
+    $defaults = {
+      'ensure'       => present,
+      'shell'        => '/bin/bash',
+      'password'     => '!!',
+      'create_group' => false,
+    }
+  } else {
     # no login allowed!
-    $defaults['shell'] = '/sbin/nologin'
+    $defaults = {
+      'ensure'       => present,
+      'shell'        => '/sbin/nologin',
+      'password'     => '!!',
+      'create_group' => false,
+    }
   }
 
   create_resources('account', $users, $defaults)
