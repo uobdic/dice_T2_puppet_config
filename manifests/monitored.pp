7695a6c6a3eda8970f1e95c710afae981e8e8e7f
kreczko <lkreczko@gmail.com>
2015-12-08 09:44:23 +0000

    creating /etc/facter directory if not existing

--- manifests/monitored.pp
+++ manifests/monitored.pp
@@ -5,9 +5,10 @@ class site::monitored ($monitored_by) {
   $fact_name = 'monitoring_server'
   $fact_value = $monitored_by
 
-  file { '/etc/facter/facts.d': ensure => directory, } ->
+  file { ['/etc/facter', '/etc/facter/facts.d']: ensure => directory, } ->
   file { "/etc/facter/facts.d/${fact_name}":
     ensure  => present,
     content => template("${module_name}/custom_fact.py.erb"),
+    mode    => 'a+x',
   }
 }
