4af397e411142d0a19a556367ed722afab33a166
Luke Kreczko <lkreczko@googlemail.com>
2016-01-03 15:00:44 +0000

    changed params to use hiera

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,16 @@
-class site::params (
-  $test_message = undef) {
-  $cluster           = 'DICE'
+class site::params {
+  $cluster = hiera('cluster_name', 'DICE')
   # CVMFS specific parameters for the site
-  $cvmfs_version     = present # or latest if you have faith
-  $cvmfs_quota_limit = 20000
-  $cvmfs_http_proxy  = 'http://squid.example.org:3128'
-  $cvmfs_server_url  = 'http://web.example.org:80/opt/example'
-  $cvmfs_cache_base  = '/var/cache/cvmfs2'
-  $user_accounts     = undef
-  $user_groups       = undef
+  $cvmfs_version = hiera('cvmfs_version', present) # or latest if you have faith
+  $cvmfs_quota_limit = hiera('cvmfs_quota_limit', 20000)
+  $cvmfs_http_proxy = hiera('cvmfs_http_proxy', 'http://squid.example.org:3128')
+  $cvmfs_server_url = hiera('cvmfs_server_url', 'http://web.example.org:80/opt/example')
+  $cvmfs_cache_base = hiera('cvmfs_cache_base', '/var/cache/cvmfs2')
+  $user_accounts = undef
+  $user_groups = undef
 
-  $major_release     = regsubst($::operatingsystemrelease, '^(\d+)\.\d+$', '\1')
-  $repositories      = {
+  $major_release = regsubst($::operatingsystemrelease, '^(\d+)\.\d+$', '\1')
+  $repositories = {
     'epel' => {
       descr       => "Extra Packages for Enterprise Linux 6${$major_release}",
       mirrorlist  => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=x86_64',
@@ -20,31 +19,16 @@ class site::params (
       enabled     => 0,
       includepkgs => 'cvmfs,cvmfs-keys',
       priority    => 80,
-      require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM']
+      require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM'],
     }
   }
+  
+  
+  # this will have to move
+  $basic_packages = hiera_array('basic_packages', ['nano', 'yum', 'git', 'wget', 'man']) 
 
-  $for_testing       = 'default'
-
-  if $test_message != undef {
-    $for_testing = $test_message
-  }
-
-  $java_repository = 'bristol'
-  $java_version    = '1.6.0_43-fcs'
-
-  $basic_packages  = {
-    'nano'    => {},
-    'yum'     => {},
-    'git'     => {},
-    'wget'    => {},
-    'mlocate' => {},
-    'man'     => {},
-  }
-
-  $syslog          = $major_release ? {
+  $syslog = $major_release ? {
     '5'     => 'syslog',
     '6'     => 'rsyslog',
     default => 'syslog',
-  }
-}
+  } }
